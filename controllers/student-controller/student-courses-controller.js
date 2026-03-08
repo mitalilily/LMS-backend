2a52baa389c14e6ff7ef251ed6b0e0a84310cdcb
mitalilily <mitalilily8877@gmail.com>
2026-03-08 21:22:12 +0530

    Fix student course routes and controller mismatch

--- controllers/student-controller/student-courses-controller.js
+++ controllers/student-controller/student-courses-controller.js
@@ -3,19 +3,28 @@ const StudentCourses = require("../../models/StudentCourses");
 const getCoursesByStudentId = async (req, res) => {
   try {
     const { studentId } = req.params;
+
     const studentBoughtCourses = await StudentCourses.findOne({
       userId: studentId,
     });
+
+    if (!studentBoughtCourses) {
+      return res.status(404).json({
+        success: false,
+        message: "No courses found for this student",
+      });
+    }
 
     res.status(200).json({
       success: true,
       data: studentBoughtCourses.courses,
     });
   } catch (error) {
-    console.log(error);
+    console.error("Error fetching student courses:", error);
+
     res.status(500).json({
       success: false,
-      message: "Some error occured!",
+      message: "Some error occurred!",
     });
   }
 };
--- routes/student-routes/student-courses-routes.js
+++ routes/student-routes/student-courses-routes.js
@@ -1,10 +1,12 @@
 const express = require("express");
+
 const {
   getCoursesByStudentId,
 } = require("../../controllers/student-controller/student-courses-controller");
 
 const router = express.Router();
 
+// Get courses purchased by a student
 router.get("/get/:studentId", getCoursesByStudentId);
 
 module.exports = router;
